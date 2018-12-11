1c230a8a55c6e03f2de007a3cc9fa6ba53d443f4
Glen Buck <glenbuck503@gmail.com>
2018-12-10 17:39:19 -0800

    Added String Exercise

---
+++ StringExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StringExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Glen";
+            string age = "33";
+            string location = "Portland,OR";
+
+            location = location.ToUpper();
+
+            Console.WriteLine(age + name + location);
+
+            Console.WriteLine("Hello my name is " + name + " .I am " + age + " and I am from " + location);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Today was cold");
+            sb.Append(" It was 33 degrees outside and I didnt wear a jacket.");
+            sb.Append(" Tomorrow I will wear one.");
+              
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
